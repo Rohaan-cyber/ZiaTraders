1e9c4771d4181a4ab7af1ace2f9aa4f0fb5f3744
Rohaan-cyber <rohaan08es14@gmail.com>
2025-09-08 23:14:46 +0400

    Update Sale-Order.js

--- models/Sale-Order.js
+++ models/Sale-Order.js
@@ -106,7 +106,6 @@ SaleOrder.prototype.createSaleOrder = function () {
 
 
                 let customer = await SupplierCollection().findOne({ custName: supplierName })
-                console.log("supplierName:", supplierName);
                 let custPaydue = customer.custPayDue || 0
                 let total = custPaydue - this.data.kulSafiRaqm
                 await SupplierCollection().findOneAndUpdate({ custName: supplierName }, { $set: { custPayDue: total } })
