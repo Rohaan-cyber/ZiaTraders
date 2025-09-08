fe8fbc2e5befc3df5258a3cbc616dadc522c3d9f
Rohaan-cyber <rohaan08es14@gmail.com>
2025-09-08 21:47:23 +0400

    Update Sale-Order.js

--- models/Sale-Order.js
+++ models/Sale-Order.js
@@ -62,7 +62,7 @@ SaleOrder.prototype.validate = async function () {
 
 
 // Create purchase order
-SaleOrder.prototype.createsaleOrder = function () {
+SaleOrder.prototype.createSaleOrder = function () {
     return new Promise(async (resolve, reject) => {
         try {
             await this.validate()
